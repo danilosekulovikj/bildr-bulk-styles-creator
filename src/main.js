0f22d23155924d01cf55d58aa13d597f4a19369e
Danilo Sekulovikj <61990984+danilosekulovikj@users.noreply.github.com>
2023-03-04 21:03:51 +0000

    Adds persistance

--- src/main.js
+++ src/main.js
@@ -4,22 +4,61 @@ import { bulkImportStylesButton } from "./components/createBulkCreateStylesButto
 import { openModal } from "./utils/openModal";
 import { aceEditorObj } from "./components/aceEditor";
 
+if (!localStorage.getItem("bccPluginEnabledFor")) {
+  localStorage.setItem("bccPluginEnabledFor", JSON.stringify([]));
+}
+
+let projectId = window.baseSelProjID;
+let enabledForProjects = JSON.parse(
+  localStorage.getItem("bccPluginEnabledFor")
+);
+let observerRunning = false;
+
 var BildrBulkClassCreator;
 (function (BildrBulkClassCreator) {
   class BildrBulkClassCreatorPlugin extends Bildr.plugins.PluginBase {
     constructor() {
       super(
         "BildrBulkClassDev",
-        "https://p27b199c99d034975afab252b7f1086c4.bildr.com/"
+        "https://p27b199c99d034975afab252b7f1086c4.bildr.com/dev"
       );
       this.addAction("hidePlugin", () => {
         this.hide();
       });
       this.addAction("enablePlugin", () => {
-        observer.observe(targetNode, config);
+        if (enabledForProjects.indexOf(projectId) === -1) {
+          enabledForProjects.push(projectId);
+          localStorage.setItem(
+            "bccPluginEnabledFor",
+            JSON.stringify(enabledForProjects)
+          );
+        }
+        if (!observerRunning) {
+          observer.observe(targetNode, config);
+          observerRunning = true;
+        }
       });
       this.addAction("disablePlugin", () => {
-        observer.disconnect();
+        let index = enabledForProjects.indexOf(projectId);
+        if (index > -1) {
+          enabledForProjects.splice(index, 1);
+        }
+        localStorage.setItem(
+          "bccPluginEnabledFor",
+          JSON.stringify(enabledForProjects)
+        );
+        if (observerRunning && enabledForProjects.indexOf(projectId) === -1) {
+          // check if observer should be stopped
+          observer.disconnect();
+          observerRunning = false;
+        }
+      });
+      this.addAction("checkIfProjectExist", () => {
+        if (enabledForProjects && enabledForProjects.indexOf(projectId) > -1) {
+          if (!observerRunning) {
+            return;
+          }
+        }
       });
     }
   }

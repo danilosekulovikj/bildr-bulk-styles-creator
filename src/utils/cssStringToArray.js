fbca95ccb61d8ca29bd7f112ce85d038c3abbf55
Danilo Sekulovikj <61990984+danilosekulovikj@users.noreply.github.com>
2023-03-09 22:54:22 +0000

    Working for pseudo selectors with small bugs - States are not adding fully

--- src/utils/cssStringToArray.js
+++ src/utils/cssStringToArray.js
@@ -1,3 +1,40 @@
+// import { performActions } from "./performActions";
+
+// export function cssStringToArray(rawCSS) {
+//   const cssString = rawCSS;
+//   if (!cssString) return;
+
+//   const rules = cssString.split("}");
+//   const result = [];
+
+//   for (let i = 0; i < rules.length; i++) {
+//     const rule = rules[i].trim();
+//     if (rule.length === 0) continue;
+
+//     const parts = rule.split("{");
+//     const selector = parts[0].trim().replace(/^\./, "");
+//     const attributesString = parts[1].replace(/[\n\r]/g, "").trim();
+//     const attributesArray = attributesString
+//       .split(";")
+//       .filter(Boolean)
+//       .map((attribute) => {
+//         const [propertyName, value] = attribute.split(":");
+//         return {
+//           propertyName: propertyName.trim(),
+//           value: value.trim(),
+//           newProperty: "0",
+//         };
+//       });
+
+//     result.push({
+//       selector,
+//       attributes: attributesArray,
+//     });
+//   }
+//   performActions(result);
+//   return result;
+// }
+
 import { performActions } from "./performActions";
 
 export function cssStringToArray(rawCSS) {
@@ -26,10 +63,36 @@ export function cssStringToArray(rawCSS) {
         };
       });
 
-    result.push({
-      selector,
-      attributes: attributesArray,
-    });
+    const pseudoSelectorIndex = selector.indexOf(":");
+    if (pseudoSelectorIndex !== -1) {
+      const pseudoSelectorName = selector.substring(pseudoSelectorIndex + 1);
+      const mainSelectorName = selector.substring(0, pseudoSelectorIndex);
+      const mainSelector = result.find((s) => s.selector === mainSelectorName);
+
+      if (mainSelector) {
+        mainSelector.children.push({
+          pseudoSelectorName: pseudoSelectorName,
+          pseudoAttributes: attributesArray,
+        });
+      } else {
+        result.push({
+          selector: mainSelectorName,
+          attributes: attributesArray,
+          children: [
+            {
+              pseudoSelectorName: pseudoSelectorName,
+              pseudoAttributes: attributesArray,
+            },
+          ],
+        });
+      }
+    } else {
+      result.push({
+        selector,
+        attributes: attributesArray,
+        children: [],
+      });
+    }
   }
   performActions(result);
   return result;
--- src/utils/performActions.js
+++ src/utils/performActions.js
@@ -10,7 +10,7 @@ let bildrSocketID = generateUID();
 export async function performActions(classesArray) {
   try {
     for (const [index, classObj] of classesArray.entries()) {
-      const { selector, attributes } = classObj;
+      const { selector, attributes, children } = classObj;
 
       const response1 = await fetch("https://www.bildr.com/_/record/save/18", {
         headers: {
@@ -34,67 +34,107 @@ export async function performActions(classesArray) {
       const data1 = await response1.json();
 
       const primaryClassID = data1.obj.recs[0].id;
-      const pseudoAttributes = {
-        newProperty: "0",
-        propertyName: "selector",
-        value: `.css_${primaryClassID}:active`,
-        originalPropertyName: "selector",
-        enterManually: "",
-      };
+      const states = [{ state: "default", default: 1 }];
 
-      const response2 = await fetch("https://www.bildr.com/_/record/save/18", {
-        headers: {
-          accept: "*/*",
-          "content-type": "application/json",
-        },
-        body: JSON.stringify(
-          generateReqBody({
-            projectID: projectID,
-            revisionID: revisionID,
-            bildrSocketID: bildrSocketID,
-            selector: "active",
-            attributes: [pseudoAttributes, ...attributes],
-            fromStyleID: primaryClassID,
-          })
-        ),
-        method: "POST",
-        mode: "cors",
-        credentials: "include",
-      });
+      children.forEach(async (child) => {
+        const defaultPseudoAttributes = [
+          {
+            newProperty: "0",
+            propertyName: "selector",
+            value: `.css_${primaryClassID}:${child.pseudoSelectorName}`,
+            originalPropertyName: "selector",
+            enterManually: "",
+          },
+        ];
+        const response2 = await fetch(
+          "https://www.bildr.com/_/record/save/18",
+          {
+            headers: {
+              accept: "*/*",
+              "content-type": "application/json",
+            },
+            body: JSON.stringify(
+              generateReqBody({
+                projectID: projectID,
+                revisionID: revisionID,
+                bildrSocketID: bildrSocketID,
+                selector: child.pseudoSelectorName,
+                attributes: [
+                  ...defaultPseudoAttributes,
+                  ...child.pseudoAttributes,
+                ],
+                fromStyleID: primaryClassID,
+              })
+            ),
+            method: "POST",
+            mode: "cors",
+            credentials: "include",
+          }
+        );
 
-      const data2 = await response2.json();
+        const data2 = await response2.json();
 
-      const pseudoClassID = data2.obj.recs[0].id;
-      const pseudoClassName = data2.obj.recs[0].name;
-      const states = [
-        { state: "default", default: 1 },
-        { state: "hover", default: 1 },
-        { state: "disabled", default: 1 },
-        {
-          state: pseudoClassName,
-          styleClassID: pseudoClassID,
-        },
-      ];
+        const pseudoClassID = data2.obj.recs[0].id;
+        const pseudoClassName = data2.obj.recs[0].name;
+        // const states = [
+        //   { state: "default", default: 1 },
+        //   { state: "hover", default: 1 },
+        //   { state: "disabled", default: 1 },
+        //   {
+        //     state: pseudoClassName,
+        //     styleClassID: pseudoClassID,
+        //   },
+        // ];
 
-      const response3 = await fetch("https://www.bildr.com/_/record/save/18", {
-        headers: {
-          accept: "*/*",
-          "content-type": "application/json",
-        },
-        body: JSON.stringify(
-          generateReqBody({
-            projectID: projectID,
-            revisionID: revisionID,
-            bildrSocketID: bildrSocketID,
-            isPrimary: isPrimary,
-            attributes: attributes,
-            states: states,
-            id: primaryClassID,
-          })
-        ),
-        method: "POST",
-        mode: "cors",
-        credentials: "include",
+        //   console.log(child.pseudoSelectorName);
+        //   console.log([...defaultPseudoAttributes, ...child.pseudoAttributes]);
+        //   console.log(pseudoClassName);
+        //   console.log(pseudoClassID);
+
+        if (pseudoClassName && pseudoClassID) {
+          states.push({
+            state: pseudoClassName,
+            styleClassID: pseudoClassID,
+          });
+        }
+
+        if (
+          !states.find((state) => state.state === "hover") &&
+          child.pseudoSelectorName === "hover"
+        ) {
+          states.push({ state: "hover", default: 1 });
+        }
+
+        if (
+          !states.find((state) => state.state === "disabled") &&
+          child.pseudoSelectorName === "disabled"
+        ) {
+          states.push({ state: "disabled", default: 1 });
+        }
+
+        const response3 = await fetch(
+          "https://www.bildr.com/_/record/save/18",
+          {
+            headers: {
+              accept: "*/*",
+              "content-type": "application/json",
+            },
+            body: JSON.stringify(
+              generateReqBody({
+                projectID: projectID,
+                revisionID: revisionID,
+                bildrSocketID: bildrSocketID,
+                isPrimary: isPrimary,
+                attributes: attributes,
+                states: states,
+                id: primaryClassID,
+              })
+            ),
+            method: "POST",
+            mode: "cors",
+            credentials: "include",
+          }
+        );
       });
 
       // const data3 = await response3.json();

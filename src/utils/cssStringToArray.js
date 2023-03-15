96a1e27c7c7444de3d03e8e22778035931dd9975
Danilo Sekulovikj <danilosekulovikj@gmail.com>
2023-03-15 09:54:51 +0100

    Parses pseudoSelectors and mediaQueries correctly.
    Creates a tree of selectors and pseudo selectors with the media queries they belong to.
    
    Adds a check in performActions to see if pseudoSelectors exist before sending request

--- src/utils/cssStringToArray.js
+++ src/utils/cssStringToArray.js
@@ -5,73 +5,227 @@ export function cssStringToArray(rawCSS) {
   // Parse the CSS
   const ast = csstree.parse(rawCSS);
 
-  // Extract class selectors and their properties
-  let result = [];
-  csstree.walk(ast, {
-    visit: "Rule",
-    enter: (node) => {
-      let className;
-      let pseudoClass;
-      csstree.walk(node.prelude, {
-        visit: "ClassSelector",
-        enter: (node) => {
-          className = node.name;
-        },
-      });
-      csstree.walk(node.prelude, {
-        visit: "PseudoClassSelector",
-        enter: (node) => {
-          pseudoClass = node.name;
-        },
-      });
-      if (className) {
-        let properties = [];
-        csstree.walk(node.block, {
-          visit: "Declaration",
-          enter: (node) => {
-            properties.push({
-              property: node.property,
-              value: csstree.generate(node.value),
-            });
-          },
-        });
+  let selectorsArray = [];
 
-        const mainSelectorName = className;
-
-        // remove colon from pseudo-selector name
-        const pseudoSelectorName = pseudoClass ? pseudoClass : "";
-
-        const mainSelector = result.find(
-          (s) => s.selector === mainSelectorName
-        );
-
-        if (mainSelector && pseudoSelectorName) {
-          // check if pseudo-selector name exists
-          mainSelector.children.push({
-            pseudoSelectorName,
-            pseudoAttributes: properties,
-          });
-        } else if (!mainSelector) {
-          const newMainSelector = {
-            selector: mainSelectorName,
-            attributes: properties,
-            children: [],
-          };
-
-          // check if pseudo-selector name exists before adding initial child
-          if (pseudoSelectorName) {
-            newMainSelector.children.push({
-              pseudoSelectorName,
-              pseudoAttributes: properties,
-            });
-          }
-
-          result.push(newMainSelector);
+  // Walk through the CSS AST (Abstract Syntax Tree)
+  csstree.walk(ast, (node) => {
+    // Handle media queries
+    if (node.type === "Atrule" && node.name === "media") {
+      let mediaQuery;
+      // Extract the media query value
+      csstree.walk(node, (node) => {
+        if (node.type === "MediaFeature") {
+          mediaQuery = node.value.value;
         }
-      }
-    },
+      });
+
+      // Walk through the media query node
+      csstree.walk(node, (node) => {
+        if (node.type === "Rule") {
+          // Collect the attributes for the rule
+          let attributes = collectAttributes(node);
+
+          // Walk through the rule node
+          csstree.walk(node, (node) => {
+            if (node.type === "Selector") {
+              // Process the selector node
+              let selector = processSelector(node);
+              // Assign the attributes to the selector
+              selector.attributes = attributes;
+
+              // Find the primary selector index
+              let primaryIndex = findPrimarySelectorIndex(
+                selectorsArray,
+                selector
+              );
+
+              // If primaryIndex is -1, it means the selector is not in the array yet
+              if (primaryIndex === -1) {
+                primaryIndex =
+                  selectorsArray.push({
+                    selector: selector.selector,
+                  }) - 1;
+              }
+
+              // If the selector has a pseudoName, handle the pseudo selector
+              if (selector.pseudoName) {
+                // Find the pseudo selector index
+                let pseudoIndex = findPseudoSelectorIndex(
+                  selectorsArray[primaryIndex].pseudoSelectors,
+                  selector
+                );
+
+                // If pseudoIndex is -1, it means the pseudo selector is not in the array yet
+                if (pseudoIndex === -1) {
+                  // Add the media query and attributes to the new pseudo selector
+                  selector.mediaQueries = [
+                    { name: mediaQuery, attributes: attributes },
+                  ];
+                  delete selector.selector;
+                  selectorsArray[primaryIndex].pseudoSelectors.push(selector);
+                } else {
+                  // If the pseudo selector does not have a mediaQueries array, create one
+                  if (
+                    !selectorsArray[primaryIndex].pseudoSelectors[pseudoIndex]
+                      .mediaQueries
+                  ) {
+                    selectorsArray[primaryIndex].pseudoSelectors[
+                      pseudoIndex
+                    ].mediaQueries = [];
+                  }
+                  // Add the media query and attributes to the existing pseudo selector
+                  selectorsArray[primaryIndex].pseudoSelectors[
+                    pseudoIndex
+                  ].mediaQueries.push({
+                    name: mediaQuery,
+                    attributes: attributes,
+                  });
+                }
+              } else {
+                // If the selector does not have a mediaQueries array, create one
+                if (!selectorsArray[primaryIndex].mediaQueries) {
+                  selectorsArray[primaryIndex].mediaQueries = [];
+                }
+                // Add the media query and attributes to the existing selector
+                selectorsArray[primaryIndex].mediaQueries.push({
+                  name: mediaQuery,
+                  attributes: attributes,
+                });
+              }
+            }
+          });
+        }
+      });
+    }
+    // Handle non-media query rules
+    else if (node.type === "Rule") {
+      let attributes = collectAttributes(node);
+
+      // Walk through the rule node
+      csstree.walk(node, (node) => {
+        if (node.type === "Selector") {
+          // Process the selector node
+          let selector = processSelector(node);
+          // Assign the attributes to the selector
+          selector.attributes = attributes;
+
+          // Find the primary selector index
+          let primaryIndex = findPrimarySelectorIndex(selectorsArray, selector);
+
+          // If primaryIndex is -1, it means the selector is not in the array yet
+          if (primaryIndex === -1) {
+            // If the selector has a pseudoName, create a new pseudoSelectors array
+            if (selector.pseudoName) {
+              selector.pseudoSelectors = [{ pseudoName: selector.pseudoName }];
+              delete selector.pseudoName;
+            }
+            // Add the selector to the selectors array
+            selectorsArray.push(selector);
+          } else {
+            // If the selector has a pseudoName, handle the pseudo selector
+            if (selector.pseudoName) {
+              // If the primary selector does not have a pseudoSelectors array, create one
+              if (!selectorsArray[primaryIndex].pseudoSelectors) {
+                selectorsArray[primaryIndex].pseudoSelectors = [];
+              }
+              // Find the pseudo selector index
+              let pseudoIndex = findPseudoSelectorIndex(
+                selectorsArray[primaryIndex].pseudoSelectors,
+                selector
+              );
+              // If pseudoIndex is -1, it means the pseudo selector is not in the array yet
+              if (pseudoIndex === -1) {
+                selectorsArray[primaryIndex].pseudoSelectors.push({
+                  pseudoName: selector.pseudoName,
+                  attributes: selector.attributes,
+                });
+              }
+            }
+          }
+        }
+      });
+    }
   });
 
-  performActions(result);
-  return result;
+  // Function to process the selector node
+  function processSelector(node) {
+    let selector = {};
+    csstree.walk(node, (node) => {
+      if (node.type === "ClassSelector") {
+        selector.selector = node.name;
+      }
+
+      if (node.type === "PseudoClassSelector") {
+        if (node.name) {
+          // Handle any pseudo selectors that have attributes
+          if (node.children) {
+            node.children.forEach((childNode) => {
+              if (childNode.type === "Declaration") {
+                if (!selector.attributes) {
+                  selector.attributes = [];
+                }
+                let attribute = {
+                  property: childNode.property,
+                  value: csstree.generate(childNode.value),
+                  important: childNode.important,
+                };
+                selector.attributes.push(attribute);
+              }
+            });
+          }
+          // Set the pseudo selector name
+          selector.pseudoName = node.name;
+        }
+      }
+    });
+    return selector;
+  }
+
+  // Function to find the primary selector index in the selectors array
+  function findPrimarySelectorIndex(selectorsArray, selector) {
+    for (let i = 0; i < selectorsArray.length; i++) {
+      if (selectorsArray[i].selector === selector.selector) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  // Function to find the pseudo selector index in the pseudoSelectors array
+  function findPseudoSelectorIndex(pseudoSelectorsArray, selector) {
+    if (!pseudoSelectorsArray) {
+      return -1;
+    }
+
+    for (let i = 0; i < pseudoSelectorsArray.length; i++) {
+      if (pseudoSelectorsArray[i].pseudoName === selector.pseudoName) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  // Function to collect attributes for a given rule
+  function collectAttributes(rule) {
+    let attributes = [];
+
+    csstree.walk(rule, (node) => {
+      if (node.type === "Declaration") {
+        let attribute = {
+          property: node.property,
+          value: csstree.generate(node.value),
+          important: node.important,
+        };
+        attributes.push(attribute);
+      }
+    });
+
+    return attributes;
+  }
+
+  selectorsArray = selectorsArray.filter((obj) => "selector" in obj);
+
+  console.log(selectorsArray);
+  performActions(selectorsArray);
+  return selectorsArray;
 }
--- src/utils/performActions.js
+++ src/utils/performActions.js
@@ -10,7 +10,7 @@ let bildrSocketID = generateUID();
 export async function performActions(classesArray) {
   try {
     for (const [index, classObj] of classesArray.entries()) {
-      const { selector, attributes, children } = classObj;
+      const { selector, attributes, pseudoSelectors } = classObj;
 
       const response1 = await fetch("https://www.bildr.com/_/record/save/18", {
         headers: {
@@ -40,99 +40,103 @@ export async function performActions(classesArray) {
         { state: "disabled", default: 1 },
       ];
       let promises = [];
+      if (pseudoSelectors) {
+        pseudoSelectors.forEach(async (child) => {
+          const defaultPseudoAttributes = [
+            {
+              newProperty: "0",
+              propertyName: "selector",
+              value: `.css_${primaryClassID}:${child.pseudoName}`,
+              originalPropertyName: "selector",
+              enterManually: "",
+            },
+          ];
 
-      children.forEach(async (child) => {
-        const defaultPseudoAttributes = [
-          {
-            newProperty: "0",
-            propertyName: "selector",
-            value: `.css_${primaryClassID}:${child.pseudoSelectorName}`,
-            originalPropertyName: "selector",
-            enterManually: "",
-          },
-        ];
-
-        const pseudoClassPromise = new Promise(async (resolve, reject) => {
-          try {
-            const response2 = await fetch(
-              "https://www.bildr.com/_/record/save/18",
-              {
-                headers: {
-                  accept: "*/*",
-                  "content-type": "application/json",
-                },
-                body: JSON.stringify(
-                  generateReqBody({
-                    projectID: projectID,
-                    revisionID: revisionID,
-                    bildrSocketID: bildrSocketID,
-                    selector: child.pseudoSelectorName,
-                    attributes: [
-                      ...defaultPseudoAttributes,
-                      ...child.pseudoAttributes,
-                    ],
-                    fromStyleID: primaryClassID,
-                  })
-                ),
-                method: "POST",
-                mode: "cors",
-                credentials: "include",
-              }
-            );
-
-            const data2 = await response2.json();
-
-            const pseudoClassID = data2.obj.recs[0].id;
-            const pseudoClassName = data2.obj.recs[0].name;
-
-            if (pseudoClassName && pseudoClassID) {
-              const existingState = states.find(
-                (state) => state.state === pseudoClassName
+          const pseudoClassPromise = new Promise(async (resolve, reject) => {
+            try {
+              const response2 = await fetch(
+                "https://www.bildr.com/_/record/save/18",
+                {
+                  headers: {
+                    accept: "*/*",
+                    "content-type": "application/json",
+                  },
+                  body: JSON.stringify(
+                    generateReqBody({
+                      projectID: projectID,
+                      revisionID: revisionID,
+                      bildrSocketID: bildrSocketID,
+                      selector: child.pseudoName,
+                      attributes: [
+                        ...defaultPseudoAttributes,
+                        ...child.attributes,
+                      ],
+                      fromStyleID: primaryClassID,
+                    })
+                  ),
+                  method: "POST",
+                  mode: "cors",
+                  credentials: "include",
+                }
               );
-              if (existingState) {
-                existingState.styleClassID = pseudoClassID;
-              } else {
-                states.push({
-                  state: pseudoClassName,
-                  styleClassID: pseudoClassID,
-                });
+
+              const data2 = await response2.json();
+
+              const pseudoClassID = data2.obj.recs[0].id;
+              const pseudoClassName = data2.obj.recs[0].name;
+
+              if (pseudoClassName && pseudoClassID) {
+                const existingState = states.find(
+                  (state) => state.state === pseudoClassName
+                );
+                if (existingState) {
+                  existingState.styleClassID = pseudoClassID;
+                } else {
+                  states.push({
+                    state: pseudoClassName,
+                    styleClassID: pseudoClassID,
+                  });
+                }
               }
+
+              resolve();
+            } catch (error) {
+              reject(error);
             }
-
-            resolve();
-          } catch (error) {
-            reject(error);
-          }
+          });
+          // Add the promise to the array
+          promises.push(pseudoClassPromise);
         });
-        // Add the promise to the array
-        promises.push(pseudoClassPromise);
-      });
 
-      // Wait for all the promises to resolve
-      await Promise.all(promises);
+        // Wait for all the promises to resolve
+        await Promise.all(promises);
 
-      const response3 = await fetch("https://www.bildr.com/_/record/save/18", {
-        headers: {
-          accept: "*/*",
-          "content-type": "application/json",
-        },
-        body: JSON.stringify(
-          generateReqBody({
-            projectID: projectID,
-            revisionID: revisionID,
-            bildrSocketID: bildrSocketID,
-            isPrimary: isPrimary,
-            attributes: attributes,
-            states: states,
-            id: primaryClassID,
-          })
-        ),
-        method: "POST",
-        mode: "cors",
-        credentials: "include",
-      });
+        const response3 = await fetch(
+          "https://www.bildr.com/_/record/save/18",
+          {
+            headers: {
+              accept: "*/*",
+              "content-type": "application/json",
+            },
+            body: JSON.stringify(
+              generateReqBody({
+                projectID: projectID,
+                revisionID: revisionID,
+                bildrSocketID: bildrSocketID,
+                isPrimary: isPrimary,
+                attributes: attributes,
+                states: states,
+                id: primaryClassID,
+              })
+            ),
+            method: "POST",
+            mode: "cors",
+            credentials: "include",
+          }
+        );
 
-      const data3 = await response3.json();
+        const data3 = await response3.json();
+      }
     }
     console.log("All actions complete.");
   } catch (error) {
